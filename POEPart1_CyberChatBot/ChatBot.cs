0ba4064167ad0f04d13471bd11e7ad1b8a1e0e1b
k4r1i45r <ST10485655@rcconnect.edu.za>
2026-04-10 11:54:01 +0200

    Initial Project Setup

--- POEPart1_CyberChatBot/ChatBot.cs
+++ POEPart1_CyberChatBot/ChatBot.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace POEPart1_CyberChatBot
 {
     internal class ChatBot
--- POEPart1_CyberChatBot/Program.cs
+++ POEPart1_CyberChatBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//Initial setup
+
 using POEPart1_CyberChatBot;
 
 namespace SypherChatbot
